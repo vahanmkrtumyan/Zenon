0730d34779033944b3d9e4771b57364f39c726a3
Vahan Mkrtumyan <vahanmkrtumyan@gmail.com>
2019-09-02 17:55:58 +0400

    2.9

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
-import React from 'react';
-import '../src/style/scss/main.scss'
-import Hompage from './Hompage';
-import Services from './Services';
+import React from "react";
+import { Route, Switch, Redirect } from "react-router-dom";
+import "../src/style/scss/main.scss";
+import Hompage from "./Hompage";
+import Services from "./Services";
 
-
-function App()
-{
+function App() {
   return (
     <div className="App">
-      <Hompage />
+      <Switch>
+        <Route path="/" component={Hompage} exact />
+        <Route path="/Services" component={Services} />
+        <Services />
+      </Switch>
     </div>
   );
 }
